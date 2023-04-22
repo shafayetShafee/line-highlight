83c4fd1d2fb3ca7871da8d452d03334ed830ac8e
shafayetShafee <seaborn.sicario@gmail.com>
2023-04-22 23:46:18 +0600

    feat: got the highlight directive syntax work

--- _extensions/line-highlight/line-highlight.lua
+++ _extensions/line-highlight/line-highlight.lua
@@ -10,11 +10,68 @@ local function ensureHtmlDeps()
 end
 
 
+local function isEmpty(s)
+  return s == nil or s == ''
+end
+
+
+function get_lines(text)
+  local lines = {}
+  local code = text .. "\n"
+  for line in code:gmatch("([^\r\n]*)[\r\n]") do
+    table.insert(lines, line)
+  end
+  return lines
+end
+
+
+function remove_pattern(lines, pattern)
+  local code_lines = {}
+  for _, line in ipairs(lines) do
+    if line:match(pattern) then
+      local cleaned_line = line:gsub(pattern, "")
+      table.insert(code_lines, cleaned_line)
+    else
+      table.insert(code_lines, line)
+    end
+  end
+  return table.concat(code_lines, "\n")
+end
+ 
+
+-- create escaped highlight_directive_pattern
+function escape_pattern(s)
+  local escaped = ""
+  for c in s:gmatch(".") do
+    escaped = escaped .. "%" .. c
+  end
+  return escaped
+end
+
+
+function get_lines_to_ht(cb, pattern)
+  if not cb.classes:includes("cell-output") then
+    local lines_to_ht = {}
+    pattern = pattern .. "$"
+    local code_lines = get_lines(cb.text)
+    for i, line in ipairs(code_lines) do
+      if line:match(pattern) then
+        table.insert(lines_to_ht, i)
+      end
+    end
+    return table.concat(lines_to_ht, ",")
+  end
+end
+
+
 local function highlight_source(line_number)
   -- adding line-number attrs for executable code blocks
   local source_highlighter = {
     CodeBlock = function(block)
+      local lines_to_ht = get_lines_to_ht(block, "#<<")
+      line_number = isEmpty(lines_to_ht) and line_number or lines_to_ht
       block.attributes["data-code-line-numbers"] = line_number
+      block.text = remove_pattern(get_lines(block.text), "#<<$")
       return block
     end
     }
@@ -54,12 +111,19 @@ if FORMAT == "html" then
   -- line-highlighting for syntactically formatted markdown code blocks
   -- (i.e. for non-executable code blocks)
   function CodeBlock(cb)
-    if cb.attributes["source-line-numbers"] then
-      cb.attributes["data-code-line-numbers"] = cb.attributes["source-line-numbers"]
-      return cb
+    local lines_to_ht =  get_lines_to_ht(cb, "#<<")
+    local line_number
+    if not isEmpty(lines_to_ht) then
+      line_number = lines_to_ht
+    elseif cb.attributes["source-line-numbers"] then
+      line_number = cb.attributes["source-line-numbers"]
+    else
+      line_number = ""
     end
+    cb.attributes["data-code-line-numbers"] = line_number
+    cb.text = remove_pattern(get_lines(cb.text), "#<<$")
+    return cb
   end
-
 end
 
 
